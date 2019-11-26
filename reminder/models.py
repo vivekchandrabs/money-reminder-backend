b70c95b48fd01a126a6ae572a1c17c353a36e9be
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-11-26 11:07:29 +0530

    Modify fd model and Interest model

--- reminder/models.py
+++ reminder/models.py
@@ -1,5 +1,8 @@
+from djcelery.models import PeriodicTask, IntervalSchedule, CrontabSchedule
+
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 TYPE_OF_INTEREST = (
@@ -18,12 +21,16 @@ class FDAccountDetail(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     fd_name = models.CharField(max_length=100)
     principal = models.IntegerField(default=0)
+    rate_of_interest = models.FloatField(default=0.0)
     type_of_interest = models.IntegerField(choices=TYPE_OF_INTEREST, default=1)
-    period = models.IntegerField(default=0)
     time_of_interest = models.IntegerField(choices=TIME_OF_INTEREST, default=1)
+    period = models.IntegerField(default=0)
     amount = models.FloatField(default=0.0, null=True, blank=True)
     bank = models.CharField(max_length=100, default=None, null=True, blank=True)
     branch = models.CharField(max_length=100, default=None, null=True, blank=True)
+    cron_tab = models.ForeignKey(CrontabSchedule, null=True, blank=True, on_delete=models.SET_NULL)
+    interval = models.ForeignKey(IntervalSchedule, null=True, blank=True, on_delete=models.SET_NULL)
+    periodic_task = models.ForeignKey(PeriodicTask, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.user} | {self.fd_name} | {self.principal} | {self.amount}"
@@ -32,7 +39,7 @@ class FDAccountDetail(models.Model):
 class Interest(models.Model):
     fd = models.ForeignKey(FDAccountDetail, on_delete=models.CASCADE, null=True, blank=True)
     interest_amt = models.FloatField(default=0.0, null=True, blank=True)
-    month = models.DateField(auto_now=True, null=True, blank=True)
+    month = models.DateField(auto_now_add=False, null=True, blank=True)
     is_sent = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
